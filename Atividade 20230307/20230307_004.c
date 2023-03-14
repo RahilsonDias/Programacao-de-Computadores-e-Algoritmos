5caad1b6934e854f0dd0ea419b58779b314da3ef
RahilsonDias <120350314+RahilsonDias@users.noreply.github.com>
2023-03-14 17:41:16 -0300

    Update 20230307_004.c

--- Atividade 20230307/20230307_004.c
+++ Atividade 20230307/20230307_004.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int vet[5];
+    int vet[8];
     int *ptrvet = vet;
     
-    for(int i=0; i<5; i++){
+    for(int i=0; i<8; i++){
         printf("Digite um número: ");
         scanf("%d", ptrvet);
         ptrvet += 1;
@@ -13,14 +13,14 @@ int main()
     
     ptrvet = vet;
     
-    for(int i=0; i<5; i++){
+    for(int i=0; i<8; i++){
         printf("Dobro de vet[%d]: %d\n", i, (*ptrvet)*2);
         ptrvet += 1;
     }
     
     ptrvet = vet;
     
-    for(int i=0; i<5; i++){
+    for(int i=0; i<8; i++){
         if((*ptrvet)%2 == 0){
             printf("Numero par encontrado no endereco: %p\n", ptrvet);
         }
